d381c4a9ff8b03587b006033ffa4ba14e44afd47
Practica 1 <jaimesf3r@gmail.com>
2025-11-19 14:15:35 +0100

    Ejercicio 4

---
+++ Practica 4/src/Ejercicio_1.java
@@ -0,0 +1,11 @@
+public class Ejercicio_1 {
+    public static void main(String[] args) {
+        int resultado = sumar(5,2);
+        System.out.println("El resultado es: " + resultado);
+    }
+
+    public static int sumar(int num1, int num2) {
+        int suma = num1 - num2;
+        return suma;
+    }
+}
